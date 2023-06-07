2c1351de9d59bc7b3525344f653b3854a75d7414
Guilherme Braga <crazyboysfh3@gmail.com>
2023-06-07 12:34:13 -0300

    update

--- operators.js
+++ operators.js
@@ -1,3 +1,17 @@
+console.log('1' == 1)
+console.log('1' === 1)
+console.log('3' != 3)
+console.log('3' !== 3)
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+console.log(d1 === d2)
+console.log(d1 == d2) // different address
+console.log(d1.getTime() == d2.getTime())
+
+console.log(undefined == null)
+console.log(null === undefined)
+
 const resultado = nota => nota >= 7 ? 'Aprovado': 'Reprovado'
 let nota = 1
 console.log(resultado(nota))
