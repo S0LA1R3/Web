cf422d3304328265e253a3e00d6117a6605e4317
Guilherme Braga <crazyboysfh3@gmail.com>
2023-08-30 15:18:48 -0300

    update

--- JS/Function/callBackFunction.js
+++ JS/Function/callBackFunction.js
@@ -6,3 +6,19 @@ function print(nome, indice){
 
 fabricantes.forEach(print)
 fabricantes.forEach(fabricante => console.log(fabricante))
+
+
+// Sem CallBack
+const notas = [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9]
+
+let notasBaixas = []
+for(let i in notas){
+    if(i < 7)
+        notasBaixas.push(notas[i])
+}
+
+console.log(notasBaixas)
+
+//Com CallBack
+notasBaixas = notas.filter((nota) => nota < 7)
+console.log(notasBaixas)
