10e6c9347bd0649a8728c5f4fa3ff77bee862707
Guilherme Braga <crazyboysfh3@gmail.com>
2023-08-31 10:35:06 -0300

    a

--- JS/Function/Exercises/8.js
+++ JS/Function/Exercises/8.js
@@ -1,21 +1,25 @@
 const pontuacoes = '10 20 30 40 11 2 5 3 29 51'
-let vetor = []
-let menor = 999
-let atual = ''
 
-for(let i in pontuacoes){
-    atual += pontuacoes[i]
-    if(pontuacoes[i] == ' ' || i == pontuacoes.length - 1){
-        if((Number(atual) > vetor.at(-1) || vetor.length == 0) && menor != 999){
-            vetor.push(atual)
+const record = (score) => {
+    let vetor = []
+    let menor = 999
+    let atual = ''
+    for(let i in pontuacoes){
+        atual += pontuacoes[i]
+        if(pontuacoes[i] == ' ' || i == pontuacoes.length - 1){
+            if((Number(atual) > vetor.at(-1) || vetor.length == 0) && menor != 999){
+                vetor.push(atual)
+            }
+            if(Number(atual.substring(0, atual.length - 1)) < menor)
+                menor = atual
+            atual = ''
         }
-        if(Number(atual.substring(0, atual.length - 1)) < menor)
-            menor = atual
-        atual = ''
     }
+
+    for(let i in vetor){
+        console.log(`Recorde ${Number(i) + 1}: ${vetor[i]} pontos`)
+    }
+    console.log(`Pior jogo: ${menor} pontos`)
 }
 
-for(let i in vetor){
-    console.log(`Recorde ${Number(i) + 1}: ${vetor[i]} pontos`)
-}
-console.log(`Pior jogo: ${menor} pontos`)
+record(pontuacoes)
