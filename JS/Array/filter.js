ed5cd09c46f1628c79e1f5fcba81aa9ce39f02e8
Guilherme Braga <crazyboysfh3@gmail.com>
2023-09-02 01:50:43 -0300

    update

--- JS/Array/filter.js
+++ JS/Array/filter.js
@@ -1,9 +1,20 @@
+Array.prototype.filter2 = function(callback){
+    let newArray = []
+    
+    for(let i = 0; i < this.length; i++){
+        if(callback(this[i], i, this))
+            newArray.push(this[i])
+    }
+
+    return newArray
+}
+
 const produtos = [
     {nome: 'Eiffel Lego', preco: 6000, fragil: false},
     {nome: 'Macintosh', preco: 100, fragil: true},
     {nome: 'Marreta', preco: 500, fragil: false}
 ]
 
-console.log(produtos.filter(function(e){
+console.log(produtos.filter2(function(e){
     return e.preco > 3000 || e.fragil == true
 }))
