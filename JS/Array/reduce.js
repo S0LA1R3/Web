1f5e29f934091e8ae44dd1340653f3da190368c4
Guilherme Braga <crazyboysfh3@gmail.com>
2023-09-02 02:20:07 -0300

    update

--- JS/Array/reduce.js
+++ JS/Array/reduce.js
@@ -1,3 +1,13 @@
+Array.prototype.reduce2 = function(callback){
+    let acumulador = this[0]
+
+    for(let i = 1; i < this.length; i++){
+        acumulador = callback(acumulador, this[i], i, this)
+    }
+
+    return acumulador
+}
+
 const alunos = [
     {nome: 'Shaolas', coeficiente: 8.5, bolsista: false},
     {nome: 'MaShadow', coeficiente: 7.2, bolsista: true},
@@ -6,7 +16,7 @@ const alunos = [
 ]
 
 console.log(alunos.map(a => a.coeficiente))
-const resultado = alunos.map(a => a.coeficiente).reduce(function(acumulador, atual){
+const resultado = alunos.map(a => a.coeficiente).reduce2(function(acumulador, atual){
     console.log(atual)
     return acumulador + atual
 })
