42bb4aa9142570eea6ecf634dbf44d0d89fa8869
Guilherme Braga <crazyboysfh3@gmail.com>
2023-09-02 23:32:20 -0300

    update

---
+++ JS/Node/employees/employees.js
@@ -0,0 +1,18 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+axios.get(url).then(response => {
+    let chinaWomen = Object.entries(response.data).filter(employee => {
+        if(employee[1].pais == 'China' && employee[1].genero == 'M'){
+            return true
+        }
+    })
+
+    let menor = {salario: 999999}
+    for(let i in chinaWomen){
+        if(chinaWomen[i][1].salario < menor.salario){
+            menor = chinaWomen[i][1]
+        }
+    }
+    console.log(menor)
+})
