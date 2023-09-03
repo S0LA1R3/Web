945c44ad9ca46a69aecf999d38da3e320da107b0
Guilherme Braga <crazyboysfh3@gmail.com>
2023-09-02 22:57:11 -0300

    update

--- JS/Node/thirdPartyRequire.js
+++ JS/Node/thirdPartyRequire.js
@@ -1,3 +1,6 @@
 const _ = require('lodash')
 setInterval(() => console.log(_.random(1, 1000)), 1000)
 // nodemon thirdPartyRequire.js
+
+const amogus = require('./amogus')
+console.log(amogus.amogus)
