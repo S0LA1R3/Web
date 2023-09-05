a658952f268ab40922e4d7d3cb2b9d674d5bbbf0
Guilherme Braga <crazyboysfh3@gmail.com>
2023-09-05 10:48:28 -0300

    update

--- JS/ESNext/promise.js
+++ JS/ESNext/promise.js
@@ -11,3 +11,23 @@ talkAfter(3, 'Que legal!')
     .then(phrase => phrase.concat('?!?'))
     .then(anotherPhrase => console.log(anotherPhrase))
     .catch(e => console.log(e))
+
+function error(value, err){
+    return new Promise((resolve, reject) => {
+        if(Math.random < err){
+            reject('Ocorreu um erro!')
+        }else{
+            resolve(value)
+        }
+    })
+}
+
+error('Test', 0.5).then(v => console.log(`Valor ${v}`))
+    .then(v => `Value: ${v}`)
+    .then(
+        v => consol.log(v),
+        err => console.log(`Erro: ${err}`)
+    )
+    .then(console.log('Quase fim'))
+    .catch(err => console.log(`Erro Geral: ${err}`))
+    .then(console.log('Fim!'))
