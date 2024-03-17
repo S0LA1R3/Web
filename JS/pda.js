38dd4063962819f641cc3ce9f181805364162a49
Guilherme Braga <crazyboysfh3@gmail.com>
2024-03-16 21:18:00 -0300

    dom

--- JS/pda.js
+++
@@ -1,7 +0,0 @@
-fetch("https://loginvalidatorapi-mmwp.onrender.com/")
-.then(res => res.json())
-.then(data => {
-    console.log(data)
-    console.log(JSON.stringify(data))
-    console.log(data.message.toUpperCase())
-})
